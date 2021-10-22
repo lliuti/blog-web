35c9f94cb387551539e738e0a73cceef01ebf0f4
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-22 20:13:42 -0300

    Removing sign up button
    Removing it to only authenticate users through github

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
+import { VscGithub } from "react-icons/vsc"
+import { api } from "../../services/api";
 import styles from "./styles.module.scss"
 
 export function Header() {
+  const handleGithubButton = async () => {
+    await api.get("/github");
+  };
+
   return (
     <div className={styles.headerContainer}>
-      <button className={styles.signInButton}>LOGIN</button>
-      <button className={styles.signUpButton}>CREATE ACCOUNT</button>
+      <button onClick={handleGithubButton} className={styles.signInWithGithubButton}>
+        <VscGithub size={24} />
+        LOGIN WITH GITHUB
+      </button>
     </div>
   );
 }
