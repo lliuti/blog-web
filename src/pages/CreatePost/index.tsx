9d3fcb24f32a333cf65b53089432b442e8d4d2ea
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-24 14:33:04 -0300

    CreatingPost function

--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Header } from "../../components/Header";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
 export function CreatePost() {
   const [logged, setLogged] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState("");
+  const [token, setToken] = useState("");
 
   const history = useHistory();
 
@@ -16,18 +22,43 @@ export function CreatePost() {
     setLogged(false);
     const token = localStorage.getItem("user:token");
     if (token) {
+      setToken(token)
       setLogged(true);
     } else {
       history.push("/");
     }
   };
+
+  const handleCreatePostButton = async () => {
+    setTitle(title.trim());
+    setDescription(description.trim());
+    setContent(content.trim());
+
+    const data = {
+      title,
+      description,
+      content
+    }
+
+    const response = await api.post("/posts", data, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+
+    console.log(response);
+  }
+
   return (
     <>
       <Header />
       <div className={styles.createPostWrapper}>
-        <input className={styles.titleInput} type="text" placeholder="Title" />
-        <input className={styles.descriptionInput} type="text" placeholder="Description" />
-        <textarea className={styles.contentTextarea} placeholder="Content"></textarea>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} className={styles.titleInput} type="text" placeholder="Title" />
+        <input value={description} onChange={(e) => setDescription(e.target.value)} className={styles.descriptionInput} type="text" placeholder="Description" />
+        <textarea value={content} onChange={(e) => setContent(e.target.value)} className={styles.contentTextarea} placeholder="Content"></textarea>
+        <div className={styles.buttonRow}>
+          <button onClick={handleCreatePostButton} className={styles.createPostButton}>CREATE</button>
+        </div>
       </div>
     </>
   )
